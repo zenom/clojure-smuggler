517def555315170e0ee817aa001ada37dba5d10e
Andy Holman <andy@27eleven.com>
2012-04-15 19:39:55 -0400

    Update to use clojure 1.3.

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :min-lein-version "1.7.1"
   :url "https://github.com/zenom/clojure-knapsack"
   :clean-non-project-classes true
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
 )
