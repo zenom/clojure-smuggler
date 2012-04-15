c64b1340ac8db301c5c6b840f6c5b7831b470e2b
Andy Holman <andy@27eleven.com>
2012-04-15 19:59:52 -0400

    Update with github url.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure Smugger Problem"
   :main smuggler.core
   :min-lein-version "1.7.1"
-  :url "https://github.com/zenom/clojure-knapsack"
+  :url "https://github.com/zenom/clojure-smuggler"
   :clean-non-project-classes true
   :dependencies [[org.clojure/clojure "1.3.0"]]
 )
